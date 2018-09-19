6c10f33f20c33f2406c35b8f1dc0ed0f47fc70dc
Fuli Feng <fulifeng93@gmail.com>
2018-09-19 13:51:01 +0800

    common update

--- code/model/lstm.py
+++ code/model/lstm.py
@@ -1,7 +1,12 @@
 import numpy as np
+import os
+import random
+from sklearn.utils import shuffle
+import tensorflow as tf
+from time import time
 
 from base_predictor import BasePredictor
 
 class LSTM(BasePredictor):
-    def __init__(self, parameters):
+    def __init__(self, parameters, epochs=50, batch_size=1024, ):
         BasePredictor.__init__(parameters)
