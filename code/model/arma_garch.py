39d110b1720d41269290f4e6e62bb6be39e3915e
chanmingwei <e0008992@u.nus.edu>
2018-10-05 13:34:29 +0800

    logistic regression and arma garch

---
+++ code/model/arma_garch.py
@@ -0,0 +1,34 @@
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima_model import ARMAResults
+from model.base_predictor import BasePredictor
+from arch import arch_model
+
+class ARMA_GARCH(BasePredictor):
+  def __init__(self,parameters):
+    BasePredictor.__init__(self,parameters)
+
+  def train(self, ts_train):  
+    chosen_order = (0,0,0)
+    min_aic = None
+    for p in (5, 10, 20, 40):
+      for q in range(1,6):
+        arma_model = ARIMA(ts_train, order = (p,0,q))
+        aic = ARMAResults.aic(arma_model)
+        if min_aic is None:
+          chosen_order = (p,0,q)
+          min_aic = aic
+        
+        if aic < min_aic:
+          chosen_order = (p,0,q)
+          min_aic = aic
+
+      final_model = arch_model(ts_train,p=p,q=q)
+
+
+    
+    
+    self.model = final_model
+  
+
+
+
---
+++ code/model/logistic_regression.py
@@ -0,0 +1,23 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from model.base_predictor import BasePredictor
+
+class LogReg(BasePredictor):
+  def __init__(self,parameters):
+    BasePredictor.__init__(self,parameters)
+
+  def train(self, X_tr, Y_tr):  
+    model = LogisticRegression(parameters)
+    model.fit(X_tr, Y_tr)
+
+    self.model = model
+
+  def test(self, X_tes, Y_tes):
+    pred = self.max_model.predict(X_tes)
+    return accuracy_score(Y_tes, pred)
+  
+
+
+    
+
+  
