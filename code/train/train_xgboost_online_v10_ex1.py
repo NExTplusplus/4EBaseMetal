7737bd9cc6be8dc24b6eb1fd41ba8a019526319c
chanmingwei <e0008992@u.nus.edu>
2019-09-10 14:08:00 +0800

    v10 ex1 ex2 online

--- code/train/train_xgboost_online_v10_ex1.py
+++ code/train/train_xgboost_online_v10_ex1.py
@@ -27,14 +27,14 @@ if __name__ == '__main__':
         help='configure file of the features to be read',
         default='exp/3d/Co/logistic_regression/v5/LMCADY_v5.conf'
     )
-    parser.add_argument('-s','--steps',type=int,default=5,
+    parser.add_argument('-s','--steps',type=int,default=3,
                         help='steps in the future to be predicted')
     parser.add_argument('-gt', '--ground_truth', help='ground truth column',
                         type=str, default="LME_Co_Spot")
     parser.add_argument('-max_iter','--max_iter',type=int,default=100,
                         help='max number of iterations')
     parser.add_argument(
-        '-sou','--source', help='source of data', type = str, default = "NExT"
+        '-sou','--source', help='source of data', type = str, default = "4E"
     )
     parser.add_argument(
         '-mout', '--model_save_path', type=str, help='path to save model',
@@ -47,12 +47,18 @@ if __name__ == '__main__':
         '-k','--k_folds', type=int, default = 10, help='number of folds to conduct cross validation'
     )
     parser.add_argument(
-        '-v','--version', help='version', type = str, default = 'v10'
+        '-v','--version', help='version', type = str, default = 'v7'
     )
     parser.add_argument ('-out','--output',type = str, help='output file', default ="../../../Results/results")
     parser.add_argument('-o', '--action', type=str, default='train',
                         help='train, test, tune')
-    parser.add_argument('-xgb','--xgboost',type = int,help='if you want to train the xgboost you need to inform us of that',default=0)
+    parser.add_argument('-xgb','--xgboost',type = int,help='if you want to train the xgboost you need to inform us of that',default=1)
+    parser.add_argument('-max_depth','--max_depth',type = int, help='feed the parameter into the model', default=0)
+    parser.add_argument('-learning_rate','--learning_rate',type=float,help='feed the parameter into the model', default=0)
+    parser.add_argument('-gamma','--gamma',type=float,help='feed the parameter into the model',default=0)
+    parser.add_argument('-min_child','--min_child',type=int,help='feed the parameter into the model',default=0)
+    parser.add_argument('-subsample','--subsample',type=float,help='feed the parameter into the model',default=0)
+    parser.add_argument('-voting','--voting',type=str,help='there are five methods for voting: all,far,same,near,reverse')
     args = parser.parse_args()
     if args.ground_truth =='None':
         args.ground_truth = None
@@ -61,51 +67,6 @@ if __name__ == '__main__':
     with open(os.path.join(sys.path[0],args.data_configure_file)) as fin:
         fname_columns = json.load(fin)
     args.ground_truth = args.ground_truth.split(",")
-    #print("args.ground_truth is {}".format(args.ground_truth))
-    #import os
-    #os._exit(0)
-    '''
-    if args.lag==5 and args.ground_truth[0]=='LME_Ti_Spot':
-        gamma=0.8
-        learning_rate=0.9
-        max_depth=4
-        subsample=0.9
-    elif args.lag==5 and args.ground_truth[0]=='LME_Co_Spot':
-        gamma=0.9
-        learning_rate=0.7
-        max_depth=5
-        subsample=0.85
-    elif args.lag==10 and args.ground_truth[0]=='LME_Ti_Spot':
-        gamma=0.9
-        learning_rate=0.9
-        max_depth=4
-        subsample=0.7
-    elif args.lag==10 and args.ground_truth[0]=='LME_Co_Spot':
-        gamma=0.8
-        learning_rate=0.8
-        max_depth=6
-        subsample=0.9
-    elif args.lag==20 and args.ground_truth[0]=='LME_Ti_Spot':
-        gamma=0.7
-        learning_rate=0.8
-        max_depth=4
-        subsample=0.7
-    elif args.lag==20 and args.ground_truth[0]=='LME_Co_Spot':
-        gamma=0.8
-        learning_rate=0.7
-        max_depth=4
-        subsample=0.9
-    elif args.lag==30 and args.ground_truth[0]=='LME_Ti_Spot':
-        gamma=0.7
-        learning_rate=0.8
-        max_depth=4
-        subsample=0.7
-    elif args.lag==30 and args.ground_truth[0]=='LME_Co_Spot':
-        gamma=0.7
-        learning_rate=0.7
-        max_depth=4
-        subsample=0.9
-    '''
     if args.action == 'train':
         comparison = None
         n = 0
@@ -119,13 +80,11 @@ if __name__ == '__main__':
                 from utils.read_data import read_data_v5_4E
                 time_series, LME_dates = read_data_v5_4E("2003-11-12")
             length = 5
-            split_dates = rolling_half_year("2009-07-01","2017-01-01",length)
-            split_dates  =  split_dates[:]
+            split_dates = rolling_half_year("2009-07-01","2019-01-01",length)
+            split_dates  =  split_dates[-4:]
             importance_list = []
             version_params=generate_version_params(args.version)
             for split_date in split_dates:
-                #print("the train date is {}".format(split_date[0]))
-                #print("the test date is {}".format(split_date[1]))
                 horizon = args.steps
                 norm_volume = "v1"
                 norm_3m_spread = "v1"
@@ -140,14 +99,14 @@ if __name__ == '__main__':
                 else:
                     norm_params = {'vol_norm':norm_volume,'ex_spread_norm':norm_ex,'spot_spread_norm':norm_3m_spread,
                                 'len_ma':len_ma,'len_update':len_update,'both':3,'strength':0.01,'xgboost':False}
+                tech_params = {'strength':0.01,'both':3,'Win_VSD':[10,20,30,40,50,60],'Win_EMA':12,'Win_Bollinger':22,
+                                                'Fast':12,'Slow':26,'Win_NATR':10,'Win_VBM':22,'acc_initial':0.02,'acc_maximum':0.2}
                 final_X_tr = []
                 final_y_tr = []
                 final_X_va = []
                 final_y_va = []
                 final_X_te = []
                 final_y_te = [] 
-                tech_params = {'strength':0.01,'both':3,'Win_VSD':[10,20,30,40,50,60],'Win_EMA':12,'Win_Bollinger':22,
-                                                'Fast':12,'Slow':26,'Win_NATR':10,'Win_VBM':22,'acc_initial':0.02,'acc_maximum':0.2}
                 ts = copy(time_series.loc[split_date[0]:split_date[2]])
                 i = 0
                 for ground_truth in ['LME_Co_Spot','LME_Al_Spot','LME_Ni_Spot','LME_Ti_Spot','LME_Zi_Spot','LME_Le_Spot']:
@@ -170,12 +129,9 @@ if __name__ == '__main__':
                     i+=1
                 final_X_tr = [np.transpose(arr) for arr in np.dstack(final_X_tr)]
                 final_y_tr = [np.transpose(arr) for arr in np.dstack(final_y_tr)]
-                final_X_va = [np.transpose(arr) for arr in np.dstack(final_X_va)]
-                final_y_va = [np.transpose(arr) for arr in np.dstack(final_y_va)]
                 final_X_tr = np.reshape(final_X_tr,[np.shape(final_X_tr)[0]*np.shape(final_X_tr)[1],np.shape(final_X_tr)[2]])
                 final_y_tr = np.reshape(final_y_tr,[np.shape(final_y_tr)[0]*np.shape(final_y_tr)[1],np.shape(final_y_tr)[2]])
-                final_X_va = np.reshape(final_X_va,[np.shape(final_X_va)[0]*np.shape(final_X_va)[1],np.shape(final_X_va)[2]])
-                final_y_va = np.reshape(final_y_va,[np.shape(final_y_va)[0]*np.shape(final_y_va)[1],np.shape(final_y_va)[2]])
+
                 
                 column_lag_list = []
                 column_name = []
@@ -190,58 +146,47 @@ if __name__ == '__main__':
                 column_lag_list.append("Zi")
                 column_lag_list.append("Le")
                 train_dataframe = pd.DataFrame(final_X_tr,columns=column_lag_list)
-                # train_dataframe.to_csv("all_xgb_ex1.csv")
                 train_X = train_dataframe.loc[:,column_lag_list]
                 train_y = pd.DataFrame(final_y_tr,columns=['result'])
-                
-                test_dataframe = pd.DataFrame(final_X_va,columns=column_lag_list)
-                test_X = test_dataframe.loc[:,column_lag_list] 
-                n_splits=args.k_folds
-                for max_depth in [3,4,5]:
-                    for learning_rate in [0.6,0.7,0.8,0.9]:
-                        for gamma in [0.6,0.7,0.8,0.9]:
-                            for min_child_weight in [3,4,5,6]:
-                                for subsample in [0.6,0.7,0.85,0.9]:
-                                    from sklearn.metrics import accuracy_score
-                                    model = xgb.XGBClassifier(max_depth=max_depth,
-                                                learning_rate = learning_rate,
-                                                n_estimators=500,
-                                                silent=True,
-                                                nthread=10,
-                                                gamma=gamma,
-                                                min_child_weight=min_child_weight,
-                                                subsample=subsample,
-                                                colsample_bytree=0.7,
-                                                colsample_bylevel=1,
-                                                reg_alpha=0.0001,
-                                                reg_lambda=1,
-                                                scale_pos_weight=1,
-                                                seed=1440,
-                                                missing=None)
-                                    folds = KFold(n_splits=n_splits)
-                                    print(folds.split(train_X))
-                                    scores = []
-                                    prediction = np.zeros((len(X_va), 1))
-                                    folder_index = []
-                                    for fold_n, (train_index, valid_index) in enumerate(folds.split(train_X)):
-                                        # print("the train_index is {}".format(train_index))
-                                        # print("the test_index is {}".format(valid_index))
-                                        if fold_n != 9:
-                                            continue
-                                        X_train, X_valid = train_X[column_lag_list].iloc[train_index], train_X[column_lag_list].iloc[valid_index]
-                                        y_train, y_valid = train_y.iloc[train_index], train_y.iloc[valid_index]
-                                        model.fit(X_train, y_train,eval_metric='error',verbose=True,eval_set=[(X_valid,y_valid)],early_stopping_rounds=5)
-                                        y_pred_valid = model.predict(X_valid)
-                                        y_pred = model.predict_proba(test_X, ntree_limit=model.best_ntree_limit)[:, 1]
-                                        y_pred = y_pred.reshape(-1, 1)
-                                        y_pred = [1 if y > 0.5 else 0 for y in y_pred]
-                                    #print("the lag is {}".format(lag))
-                                    print("the all folder voting precision is {}".format(metrics.accuracy_score(final_y_va, y_pred)))
-                                    print("the max_depth is {}".format(max_depth))
-                                    print("the learning_rate is {}".format(learning_rate))
-                                    print("the gamma is {}".format(gamma))
-                                    print("the min_child_weight is {}".format(min_child_weight))
-                                    print("the subsample is {}".format(subsample))
+                for i,gt in enumerate(["LMCADY","LMAHDY","LMNIDY","LMSNDY","LMZSDY","LMPBDY"]):
+                    print("ground truth is "+gt)
+                    test_dataframe = pd.DataFrame(final_X_va[i],columns=column_lag_list)
+                    test_X = test_dataframe.loc[:,column_lag_list] 
+                    n_splits=args.k_folds
+                    from sklearn.metrics import accuracy_score
+                    model = xgb.XGBClassifier(max_depth=args.max_depth,
+                                learning_rate = args.learning_rate,
+                                n_estimators=500,
+                                silent=True,
+                                nthread=10,
+                                gamma=args.gamma,
+                                min_child_weight=args.min_child,
+                                subsample=args.subsample,
+                                colsample_bytree=0.7,
+                                colsample_bylevel=1,
+                                reg_alpha=0.0001,
+                                reg_lambda=1,
+                                scale_pos_weight=1,
+                                seed=1440,
+                                missing=None)
+                    folds = KFold(n_splits=n_splits)
+                    scores = []
+                    prediction = np.zeros((len(X_va), 1))
+                    folder_index = []
+                    for fold_n, (train_index, valid_index) in enumerate(folds.split(train_X)):
+                    # print("the train_index is {}".format(train_index))
+                    # print("the test_index is {}".format(valid_index))
+                        if fold_n != 9:
+                            continue
+                        X_train, X_valid = train_X[column_lag_list].iloc[train_index], train_X[column_lag_list].iloc[valid_index]
+                        y_train, y_valid = train_y.iloc[train_index], train_y.iloc[valid_index]
+                        model.fit(X_train, y_train,eval_metric='error',verbose=True,eval_set=[(X_valid,y_valid)],early_stopping_rounds=5)
+                        y_pred_valid = model.predict(X_valid)
+                        y_pred = model.predict_proba(test_X, ntree_limit=model.best_ntree_limit)[:, 1]
+                        y_pred = y_pred.reshape(-1, 1)
+                        y_pred = [1 if y > 0.5 else 0 for y in y_pred]
+                    #print("the lag is {}".format(lag))
+                    print("the all folder voting precision is {}".format(metrics.accuracy_score(final_y_va[i], y_pred)))
                 print("the lag is {}".format(lag))
                 print("the train date is {}".format(split_date[0]))
                 print("the test date is {}".format(split_date[1]))
--- code/utils/retrieve_commands.py
+++ code/utils/retrieve_commands.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 ground_truth = ("all","all")
-version = "v10"
+version = "v10_ex2"
 with open(ground_truth[0]+" "+version+".txt","w") as out:
   for lag in [5,10,20,30]:
     for h in [1,3,5]:
       f = pd.read_csv('_'.join([ground_truth[0],version,str(lag),"h"+str(h)])+".csv")
-      out.write(" ".join(["python code/train/train_xgboost_online_v10.py",
+      out.write(" ".join(["python code/train/train_xgboost_online_v10_ex2.py",
                           # "-gt",ground_truth[1],
                           "-l",str(lag),"-s",str(h),"-xgb 0","-v",version,"-c exp/LMCADY_v5.conf",
                           "-sou NExT","-max_depth",str(int(f.iloc[0,1])),"-learning_rate",str(f.iloc[0,2]),
