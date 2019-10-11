5a1ca083018274800109db3965dc7722f7591199
chanmingwei <chanmingwei@4ecappcsg110.fourelements.sg>
2019-10-12 00:38:46 +0800

    minor change

--- code/train/train_alstm_online.py
+++ code/train/train_alstm_online.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from sklearn.metrics import accuracy_score, f1_score
 from copy import copy
-import psutil
+#import psutil
 sys.path.insert(0, os.path.abspath(os.path.join(sys.path[0], '..')))
 from data.load_data_v16_torch import load_data
 from model.model_embedding import MultiHeadAttention, attention, bilstm
@@ -23,13 +23,13 @@ random.seed(1)
 thresh = 0
 
 
-
+'''
 def memory_usage():
     pid = os.getpid()
     py = psutil.Process(pid=pid)
     memory_use = py.memory_info()[0]/2.**30
     print('memory useage:', memory_use)
-
+'''
 class Trainer:
     def __init__(self, input_dim, hidden_state, time_step, lr, dropout,
                  case_number, attention_size, embedding_size,
