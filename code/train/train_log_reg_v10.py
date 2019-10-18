a2f1c90adfd81a4d1a888a5e667919321a35c83f
chanmingwei <e0008992@u.nus.edu>
2019-10-18 14:41:30 +0800

    commented train_log_reg_v10

--- code/train/train_log_reg_v10.py
+++ code/train/train_log_reg_v10.py
@@ -109,8 +109,10 @@ if __name__ == '__main__':
     if args.action == 'train':
         comparison = None
         n = 0
+        #iterate over list of configurations
         for f in fname_columns:
             lag = args.lag
+            #read data
             if args.source == "NExT":
                 from utils.read_data import read_data_NExT
                 data_list, LME_dates = read_data_NExT(f, "2003-11-12")
@@ -127,6 +129,7 @@ if __name__ == '__main__':
             for split_date in split_dates:
                 #print("the train date is {}".format(split_date[0]))
                 #print("the test date is {}".format(split_date[1]))
+                #generate parameters for load data
                 horizon = args.steps
                 norm_volume = "v1"
                 norm_3m_spread = "v1"
@@ -134,13 +137,8 @@ if __name__ == '__main__':
                 len_ma = 5
                 len_update = 30
                 tol = 1e-7
-                if args.xgboost==1:
-                    print(args.xgboost)
-                    norm_params = {'vol_norm':norm_volume,'ex_spread_norm':norm_ex,'spot_spread_norm':norm_3m_spread,
-                                'len_ma':len_ma,'len_update':len_update,'both':3,'strength':0.01,'xgboost':True}
-                else:
-                    norm_params = {'vol_norm':norm_volume,'ex_spread_norm':norm_ex,'spot_spread_norm':norm_3m_spread,
-                                'len_ma':len_ma,'len_update':len_update,'both':3,'strength':0.01,'xgboost':False}
+                norm_params = {'vol_norm':norm_volume,'ex_spread_norm':norm_ex,'spot_spread_norm':norm_3m_spread,
+                            'len_ma':len_ma,'len_update':len_update,'both':3,'strength':0.01,'xgboost':False}
                 final_X_tr = []
                 final_y_tr = []
                 final_X_va = []
@@ -151,11 +149,14 @@ if __name__ == '__main__':
                                                 'Fast':12,'Slow':26,'Win_NATR':10,'Win_VBM':22,'acc_initial':0.02,'acc_maximum':0.2}
                 ts = copy(time_series.loc[split_date[0]:split_date[2]])
                 i = 0
+                #iterate over ground truths
                 for ground_truth in ['LME_Co_Spot','LME_Al_Spot','LME_Ni_Spot','LME_Ti_Spot','LME_Zi_Spot','LME_Le_Spot']:
                     print(ground_truth)
                     metal_id = [0,0,0,0,0,0]
                     metal_id[i] = 1
+                    #load data
                     X_tr, y_tr, X_va, y_va, X_te, y_te, norm_check,column_list = load_data(copy(ts),LME_dates,horizon,[ground_truth],lag,split_date,norm_params,tech_params,version_params)
+                    #post load processing and metal id extension
                     X_tr = np.concatenate(X_tr)
                     X_tr = X_tr.reshape(len(X_tr),lag*len(column_list[0]))
                     X_tr = np.append(X_tr,[metal_id]*len(X_tr),axis = 1)
@@ -169,6 +170,8 @@ if __name__ == '__main__':
                     final_X_va.append(X_va)
                     final_y_va.append(y_va)
                     i+=1
+                
+                #sort by time, not by metal
                 final_X_tr = [np.transpose(arr) for arr in np.dstack(final_X_tr)]
                 final_y_tr = [np.transpose(arr) for arr in np.dstack(final_y_tr)]
                 final_X_va = [np.transpose(arr) for arr in np.dstack(final_X_va)]
@@ -178,6 +181,7 @@ if __name__ == '__main__':
                 final_X_va = np.reshape(final_X_va,[np.shape(final_X_va)[0]*np.shape(final_X_va)[1],np.shape(final_X_va)[2]])
                 final_y_va = np.reshape(final_y_va,[np.shape(final_y_va)[0]*np.shape(final_y_va)[1],np.shape(final_y_va)[2]])
 
+                #tune logistic regression hyper parameter
                 for C in [0.00001,0.0001,0.001,0.01]:
                     if C not in ans['C']:
                         ans["C"].append(C)
