60b33d2aea933b48f092aa07702c85999dff72d1
chang111 <1722144381@qq.com>
2019-11-01 16:44:18 +0800

    Update train_alstm_online.py

--- code/train/train_alstm_online.py
+++ code/train/train_alstm_online.py
@@ -618,7 +618,7 @@ if __name__ == '__main__':
                 exit(0)
 
             length = 5
-            split_dates = rolling_half_year("2009-07-01","2019-01-01",length)
+            split_dates = rolling_half_year("2009-07-01","2019-07-01",length)
             # split_dates = split_dates[:]
             split_dates = split_dates[-5:-1]
             importance_list = []
