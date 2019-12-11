827871ab72f0fec991f6aabcab37ec5730180406
chang111 <1722144381@qq.com>
2019-12-11 11:11:51 +0800

    Update ALSTM_live.py

--- code/live/ALSTM_live.py
+++ code/live/ALSTM_live.py
@@ -597,7 +597,7 @@ class ALSTM_online():
 		#out_val_pred, out_test_pred, out_loss = trainer.train_minibatch(num_epochs, batch_size, interval)
 		save = 1
 		net = trainer.train_minibatch(num_epochs, batch_size, interval)
-		torch.save(net, split_dates[0]+"_"+self.gt+"_"+str(self.horizon)+"_"+str(lag)+"_"+str(fold_n)+"_"+'alstm.model')
+		torch.save(net, split_dates[0]+"_"+self.gt+"_"+str(self.horizon)+"_"+str(lag)+"_"+'alstm.model')
 	#-------------------------------------------------------------------------------------------------------------------------------------#
 	"""
 	this function is used to predict the date
@@ -824,7 +824,7 @@ class ALSTM_online():
 		test_X = torch.from_numpy(final_X_te).float()
 		test_Y = torch.from_numpy(final_y_te).float()
 		var_x_test_id = torch.LongTensor(np.array(final_test_X_embedding))
-		net = torch.load(split_dates[0]+"_"+self.gt+"_"+str(self.horizon)+"_"+str(lag)+"_"+str(fold_n)+"_"+'alstm.model')
+		net = torch.load(split_dates[0]+"_"+self.gt+"_"+str(self.horizon)+"_"+str(lag)+"_"+'alstm.model')
 		test_output = net(test_X, var_x_test_id)
 		#loss = loss_func(test_output, test_Y)
 		#loss_sum = loss.detach()
