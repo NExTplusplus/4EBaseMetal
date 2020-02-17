f9c173a336affcb50fcbe467eb6588220515a3d0
chanmingwei <e0008992@u.nus.edu>
2020-02-17 16:34:53 +0800

    test analyze_predictions

---
+++ code/utils/analyze_predictions.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+from copy import copy
+import argparse
+import os
+from sklearn.metrics import accuracy_score
+
+if __name__ == '__main__':
+    desc = 'the script for Logistic Regression'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-s','--step_list',type=str,default="1,3,5",
+                        help='list of horizons to be calculated, separated by ","')
+    parser.add_argument('-gt', '--ground_truth_list', help='list of ground truths, separated by ","',
+                        type=str, default="LME_Co_Spot,LME_Al_Spot,LME_Ni_Spot,LME_Ti_Spot,LME_Zi_Spot,LME_Le_Spot")
+    parser.add_argument(
+        '-m','--model', help='model used', type = str, default = "lr"
+    )
+    parser.add_argument(
+        '-l','--lag_list', type=str, default = "1,5,10,20,30", help='list of lags, separated by ","'
+    )
+    parser.add_argument(
+        '-v','--version_list', help='list of versions, separated by ","', type = str, default = 'v10'
+    )
+    parser.add_argument ('-out','--output',type = str, help='output file', default ="../../../Results/results")
+    parser.add_argument('-o', '--action', type=str, default='commands',
+                        help='commands, testing')
+    parser.add_argument('-d','--dates',type = str, help = "dates", default = "2014-12-31,2015-06-30,2015-12-31,2016-06-30,2016-12-31,2017-06-30,2017-12-31,2018-06-30,2018-12-31")
+    parser.add_argument('-length','--length',type = str, help = "length of each period stated in dates",default = "129,124,129,125,128")
+    parser.add_argument('-p','--path',type =str, help='path to 4EBaseMetal folder',default ='/NEXT/4EBaseMetal')
+
+    args = parser.parse_args()
+    args.step_list = args.step_list.split(",")
+    args.ground_truth_list = args.ground_truth_list.split(",")
+    args.version_list = args.version_list.split(",")
+    args.dates = args.dates.split(",")
+    args.length = [int(i) for i in args.length.split(",")]
+
+    ans = {"version":[],"horizon":[],"ground_truth":[]}
+    validation_dates = [d.split("-")[0]+"-01-01" if d[4:] == "-06-30" else d.split("-")[0]+"-07-01" for d in args.dates]
+    for version in args.version_list:
+        for h in args.step_list:
+            for gt in args.ground_truth_list:
+                for i,date in enumerate(args.dates):
+                    if "_".join([gt,date,h,version])+".csv" not in os.listdir(os.path.join("result","prediction",args.model)):
+                        continue
+                    ans["version"].append(version)
+                    ans["horizon"].append(h)
+                    ans["ground_truth"].append(gt)
+                    temp = pd.read_csv(os.path.join("result","prediction",args.model,"_".join([gt,date,h,version])+".csv"),index_col = 0)
+                    label = pd.read_csv(os.path.join("data","Label","_".join([gt,"h"+str(h),validation_dates[i],"label.csv"])),index_col = 0)
+                    accuracy = accuracy_score(label,temp)
+                    if validation_dates[i]+"_acc" not in ans.keys():
+                        ans[validation_dates[i]+"_acc"] = [accuracy]
+                        ans[valiation_dates[i]+"_length"] = [len(temp)]
+                    else:
+                        ans[validation_dates[i]+"_acc"].append(accuracy)
+                        ans[validation_dates[i]+"_length"].append(len(temp))
+    ans = pd.DataFrame(ans)
+    total_acc = None
+    for date in validation_dates:
+        if total_acc is None:
+            total_acc = ans[date+"_acc"]*ans[date+"_length"]
+        else:
+            total_acc = total_acc + ans[date+"_acc"]*ans[date+"_length"]     
+    ans["final average"] = total_acc                   
+    ans.to_csv(args.output)
+
+
